094b9f375b5a6ef4fb6eb4f8e4c0535b54312380
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-09-17 07:05:00 +0530

    committed

--- src/main/java/com/example/repository/CustomerRepository.java
+++ src/main/java/com/example/repository/CustomerRepository.java
@@ -1,9 +1,8 @@
 package com.example.repository;
-
 import com.example.domain.Customer;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
